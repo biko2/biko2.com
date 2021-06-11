2344cad630386e018e5e1fd755e613ce651214a9
Luis <luis.gil@biko2.com>
2021-06-11 22:34:16 +0200

    Add plugins, dark theme and comments on generator.sh

--- src/static/bitween-docs/generator.sh
+++ src/static/bitween-docs/generator.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+#
+# Copy all module README.md files to documentation folder.
+#
+
 list="$(find ../ -name README.md -type f)"
 
 for file in $list; do 
 
+    # Skip home.
     if [[ ( $file == "../docs/README.md") ]]; then
         continue
     fi
 
+    # static_suite readme.md.
     if [[ ( $file == "../README.md") ]]; then
         mkdir -p modules
         cp $file modules
@@ -18,5 +24,5 @@ for file in $list; do
     dir="${dir//..\//}"
 
     mkdir -p $dir
-    cp $file $dir    
+    cp $file $dir
 done
