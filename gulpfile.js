532d0675536c5e25f594b3452b52f07ce1ff1bbe
Luis <luis.gil@biko2.com>
2021-06-24 12:11:06 +0200

    Override build gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,17 @@
-var gulp = require('gulp');
-require('gulp-tasks-pleasure')(gulp);
+var gulp = require("gulp");
+require("gulp-tasks-pleasure")(gulp);
+
+// Override build task
+gulp.task(
+  "build",
+  gulp.series(
+    "sass",
+    "js",
+    "images",
+    "fonts",
+    "svg",
+    "videos",
+    "static",
+    "twigPages"
+  )
+);
