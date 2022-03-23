c5dfccbd1333e735c1d908f082ad7bca3243d573
Amaya Eguizábal <amaya.eguizabal@biko2.com>
2022-03-23 17:02:32 +0100

    landing open primera version inacabada

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ require("gulp-tasks-pleasure")(gulp);
 gulp.task(
   "build",
   gulp.series(
-    //"sass",
-    //"js",
+    "sass",
+    "js",
     "images",
-    //"fonts",
-    //"svg",
-    //"videos",
-    //"static",
-    //"twigPages"
+    "fonts",
+    "svg",
+    "videos",
+    "static",
+    "twigPages"
   )
 );
